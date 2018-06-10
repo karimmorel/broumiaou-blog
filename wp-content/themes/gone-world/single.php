68db332213e1f1a8e16889d945e11adc7912c32e
Karim Morel <karim-morel@outlook.fr>
2018-06-10 14:46:11 +0700

    Ajout du thème et UI GMaps par défaut retirée

--- wp-content/themes/gone-world/single.php
+++ wp-content/themes/gone-world/single.php
@@ -115,6 +115,323 @@ if (!empty($previous_post)){
 		function initMap() {
 
 
+			//Style des cartes
+
+			var styledMapType = new google.maps.StyledMapType([
+			{
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#ebe3cd"
+				}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#523735"
+				}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+				{
+					"color": "#f5f1e6"
+				}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.stroke",
+				"stylers": [
+				{
+					"color": "#c9b2a6"
+				}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "geometry.stroke",
+				"stylers": [
+				{
+					"color": "#dcd2be"
+				}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#ae9e90"
+				}
+				]
+			},
+			{
+				"featureType": "administrative.neighborhood",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "landscape.natural",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#dfd2ae"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#dfd2ae"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#93817c"
+				}
+				]
+			},
+			{
+				"featureType": "poi.business",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry.fill",
+				"stylers": [
+				{
+					"color": "#a5b076"
+				}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#447530"
+				}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#f5f1e6"
+				}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "labels",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "labels.icon",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#fdfcf8"
+				}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#f8c967"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.stroke",
+				"stylers": [
+				{
+					"color": "#e9bc62"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "labels",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway.controlled_access",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#e98d58"
+				}
+				]
+			},
+			{
+				"featureType": "road.highway.controlled_access",
+				"elementType": "geometry.stroke",
+				"stylers": [
+				{
+					"color": "#db8555"
+				}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#806b63"
+				}
+				]
+			},
+			{
+				"featureType": "transit",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#dfd2ae"
+				}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#8f7d77"
+				}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "labels.text.stroke",
+				"stylers": [
+				{
+					"color": "#ebe3cd"
+				}
+				]
+			},
+			{
+				"featureType": "transit.station",
+				"elementType": "geometry",
+				"stylers": [
+				{
+					"color": "#dfd2ae"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry.fill",
+				"stylers": [
+				{
+					"color": "#b9d3c2"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text",
+				"stylers": [
+				{
+					"visibility": "off"
+				}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+				{
+					"color": "#92998d"
+				}
+				]
+			}
+			],
+			{name: 'Maps Jdecalle'});
+
+
+
+			//Intialiser toutes les cartes
+
 			<?php 
 			$object_itineraire = get_field('itineraire'); 
 			$parcours_list = array();
@@ -138,8 +455,18 @@ if (!empty($previous_post)){
 			var directionsService = new google.maps.DirectionsService;
 			var map = new google.maps.Map(document.getElementById('main-map'), {
 				zoom: 14,
-				center: start
+				center: start,
+				disableDefaultUI: true,
+				mapTypeControlOptions: {
+					mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+					'styled_map']
+				},
 			});
+
+			map.mapTypes.set('styled_map', styledMapType);
+			map.setMapTypeId('styled_map');
+
+
 			directionsDisplay.setMap(map);
 
 			calculateAndDisplayRoute(directionsService, directionsDisplay, start, end);
@@ -154,8 +481,18 @@ if (!empty($previous_post)){
 				var directionsService<?php echo $key; ?> = new google.maps.DirectionsService;
 				var map = new google.maps.Map(document.getElementById('article-map-<?php echo $key; ?>'), {
 					zoom: 14,
-					center: start
+					center: start,
+					disableDefaultUI: true,
+					mapTypeControlOptions: {
+						mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+						'styled_map']
+					},
 				});
+
+				map.mapTypes.set('styled_map', styledMapType);
+				map.setMapTypeId('styled_map');
+
+
 				directionsDisplay<?php echo $key; ?>.setMap(map);
 
 				calculateAndDisplayRoute(directionsService<?php echo $key; ?>, directionsDisplay<?php echo $key; ?>, start<?php echo $key; ?>, end<?php echo $key; ?>);
