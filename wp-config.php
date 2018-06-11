3683b73cf424f36ff64f3210fe52363d8d7d2c62
Karim Morel <karim-morel@outlook.fr>
2018-06-11 16:50:40 +0200

    wp-config Local

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define('DB_NAME', 'blog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'PEPITO@0711!gg');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
